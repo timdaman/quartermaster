baed10a31e8009939e4fee194c7db9402781ec8d
Tim Laurence <tim.laurence@thalmic.com>
2020-03-06 19:12:11 -0500

    Automatically register urls from apps

--- quartermaster_server/quartermaster/urls.py
+++ quartermaster_server/quartermaster/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from importlib import import_module
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -20,9 +23,17 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('api/', include('api.urls')),
-    path('gui/', include('gui.urls')),
-    path('client/', include('client.urls')),
-    # path('teamcity/', include('teamcity.urls')),
     path('', RedirectView.as_view(url='/gui/resource/'), name='index'),
 ]
+
+# Automatically register all enabled apps
+for app in settings.INSTALLED_APPS:
+    if app.startswith('django.contrib.'):
+        continue
+    app_shortname = app.split('.')[-1]
+    try:
+        _module = import_module(f"{app}.urls")
+    except:
+        pass
+    else:
+        urlpatterns.insert(0,path(f"^{app_shortname}/", include(f"{app_shortname}.urls")))
