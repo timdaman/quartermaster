dba9fc68cbbb502add7502e65c5e4ede8242967f
Tim Laurence <tim.laurence@bynorth.com>
2020-04-18 17:54:15 -0400

    Moving client to it's own repo

--- client/quartermaster_client/__main__.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if sys.version_info[0] != 3 or sys.version_info[1] < 7:
-    print("You must run this program using Python 3 version 3.7 or higher")
-    exit(1)
-
-from client.app import main
-
-main(sys.argv[1:])
